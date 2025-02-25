d673785d608653e8f1ae561f36f21cc50df2fbe6
Rithvik-23BRS1377 <rithvikvit@gmail.com>
2025-02-25 22:41:31 +0530

    Create q5app.js

---
+++ Assignment-11/Output_Screenshots/q5app.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+
+
+
+function App() {
+  const fruits=["Apple","Banana","Cherry"];
+  return(
+    <ul>
+ {fruits.map((fruit, index) => (
+ <li key={index}>{fruit}</li>
+ ))}
+ </ul>
+  )
+}
+
+export default App;
